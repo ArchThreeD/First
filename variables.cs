0d1e8e75925d12c4f5aa49e35c07a831f9214ce5
ArchThreeD <105653915+ArchThreeD@users.noreply.github.com>
2022-08-25 18:16:37 +0300

    Add files via upload

---
+++ variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte maxBuildings = 230 ;
+            sbyte theHappinessOfTheSettlement = 1;
+            int maximumNumberOfTroops = 550;
+            double constructionSpeed = 1.5;
+            string heroName = "Arch";
+            long maxDuty = -100000;
+            bool hunting = true;
+            float viewingDistance = 31.2f;
+            ulong amountOfGold = 1000000;
+            short numberOfCitizens = 650;
+                }
+        
+    }
+}
