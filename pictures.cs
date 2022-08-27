aa2b7a57fda06407dcd69e620527f2d3f3ed08da
ArchThreeD <105653915+ArchThreeD@users.noreply.github.com>
2022-08-27 20:43:30 +0300

    Update pictures.cs

--- pictures.cs
+++ pictures.cs
@@ -13,7 +13,7 @@ namespace picture
             int numberOfPicturesInRow = 3;
             int pictures = Convert.ToInt32(Console.ReadLine());
             int numberOfRows = pictures / 3;
-            float remainder = pictures % 3;
+            int remainder = pictures % 3;
             Console.WriteLine(numberOfRows);
             Console.WriteLine(remainder);
         }
