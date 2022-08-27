899f26a8255a453e6915ebe1409c3ad952297be5
ArchThreeD <105653915+ArchThreeD@users.noreply.github.com>
2022-08-27 20:42:12 +0300

    Add files via upload

---
+++ pictures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace picture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPicturesInRow = 3;
+            int pictures = Convert.ToInt32(Console.ReadLine());
+            int numberOfRows = pictures / 3;
+            float remainder = pictures % 3;
+            Console.WriteLine(numberOfRows);
+            Console.WriteLine(remainder);
+        }
+    }
+}
