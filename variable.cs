5a2ebb69c577d7caf2e25bc8293f610e8f3e00a8
ArchThreeD <105653915+ArchThreeD@users.noreply.github.com>
2022-09-01 15:23:43 +0300

    Add files via upload

---
+++ variable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Porshin";
+            string lastName = "Vasya";
+            string swapString;
+            Console.WriteLine($"{firstName}  {lastName}");
+            swapString = firstName;
+            firstName = lastName; 
+            lastName = swapString;
+            Console.WriteLine($"{firstName}  {lastName}");
+        } 
+    }
+}
