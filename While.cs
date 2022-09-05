769268d257108ea1fb5172f1a51e81d92b9469d0
ArchThreeD <105653915+ArchThreeD@users.noreply.github.com>
2022-09-05 23:31:44 +0300

    Add files via upload

---
+++ While.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+
+            while (age-- > 0 )
+            {
+                Console.WriteLine("Здравствуйте");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
