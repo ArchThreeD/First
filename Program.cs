fbc36378e3ae8ac18dc4b1d6214ea4894ea55d17
ArchThreeD <105653915+ArchThreeD@users.noreply.github.com>
2022-09-29 20:31:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            string lineWidth = "";
+
+            Console.Write("Введите свое имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine($"Ваше имя {name} . Длина вашего имени {name.Length}");
+            for (int i = 0; i < name.Length; i++)
+            {
+                lineWidth += symbol;         
+            }
+            Console.WriteLine($"{symbol}{lineWidth}{symbol}\n{symbol}{name}{symbol}\n{symbol}{lineWidth}{symbol}");
+        }
+    }
+}
